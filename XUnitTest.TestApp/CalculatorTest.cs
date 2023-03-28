0a15fbbebce096dfc5dc64f643cafe950b39fe49
Eray Bakır <info@eraybakir.com>
2023-03-28 10:50:53 +0300

    Test Project was created

---
+++ XUnitTest.TestApp/CalculatorTest.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using XUnitTest.App;
+
+namespace XUnitTest.TestApp
+{
+    //Class isimlendirmesi test işlemlerinde önemlidir
+    public class CalculatorTest
+    {
+        [Fact] //Test metodu olduğunu belirtiyoruz
+        public void AddTest()
+        {
+            //Arrange --> Initilaizer işlemleri
+            int a = 5;
+            int b = 20;
+            var calculator = new Calculator();
+
+            //Act --> Test edilecek metotlar çalıştırılır
+            var total = calculator.add(a, b);
+
+            //Assert --> Doğrulama evresi
+            //Beklenen ve aslında elde ettiğimiz değerleri veriyoruz
+            Assert.Equal(25, total);
+        }
+    }
+}
