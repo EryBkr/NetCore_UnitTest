d5ea7aa2139f2f49f274779d633a3f17c37af34e
Eray Bakır <info@eraybakir.com>
2023-03-29 00:55:32 +0300

    Moq Framework was added

---
+++ XUnitTest.App/Calculator.cs
@@ -0,0 +1,23 @@
+﻿namespace XUnitTest.App
+{
+    //Moq Framework'ün kullanılabilmesi için soyutlama yapmak gerekmektedir.Biz de Calculator class'ının kullanılışını bu şekilde clean hale getirdik
+    public class Calculator
+    {
+        private ICalculatorService _calculatorService;
+
+        public Calculator(ICalculatorService calculatorService)
+        {
+            _calculatorService = calculatorService;
+        }
+
+        public int add(int a, int b)
+        {
+            return _calculatorService.add(a, b);
+        }
+
+        public int multip(int a, int b)
+        {
+            return _calculatorService.multip(a, b);
+        }
+    }
+}
